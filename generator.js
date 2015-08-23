3cba71e0344f18ee50d6c009092c06b17d9b9d42
Casper Lai <casper.lai@sleepingdesign.com>
2015-08-23 22:04:19 +0800

    Write icd10 json to file.

--- generator.js
+++ generator.js
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import _ from 'lodash';
+import Immutable from 'immutable';
 import json from 'JSON2';
 
 export default class generator {
@@ -7,31 +9,73 @@ export default class generator {
   }
 
   generate() {
-    const pedestrian = this.compilePedestrian();
-    const pedestrianDetail = this.compilePedestrianDetail();
-    const perpetrator = this.compilePerpetrators();
-    const accitdentType = this.compileAccidentType();
-    const medical = this.compileMedical();
+    this.compilePedestrian();
+    this.compilePedestrianDetail();
+    this.compilePerpetrators();
+    this.compileAccidentType();
+    this.compileMedical();
+    this.compileICD();
+  }
+
+  /**
+   * Compile ICD-10-cm data
+   * @return void
+   */
+  compileICD() {
+    let data = Immutable.Set();
+    _.each(this.rows, (value) => {
+
+      let dest = {
+        code: value[1],
+        content: value[2],
+        pedestrian: _.get(this.pedestrian, value[5]),
+        pedestrianDetail: _.get(this.pedestrianDetail, value[6]),
+        perpetrator: _.get(this.perpetrator, value[7]),
+        accitdentType: _.get(this.accitdentType, value[8]),
+        medical: _.get(this.medical, value[9]),
+      };
+      data = data.add(dest);
+    });
+    this.icd10 = data;
+  }
+
+  make(path) {
+    const data = {
+      data: this.icd10,
+      pedestrian: this.pedestrian,
+      pedestrianDetail: this.pedestrianDetail,
+      perpetrator: this.perpetrator,
+      accitdentType: this.accitdentType,
+      medical: this.medical,
+    }
+    fs.writeFile(path, json.stringify(data), (error) => {
+      if (error) {
+        console.log('error: ' + error);
+        return false;
+      }
+
+      console.log('Compile success.');
+    });
   }
 
   compilePedestrian() {
-    return this.compile(5);
+    this.pedestrian = this.compile(5);
   }
 
   compilePedestrianDetail() {
-    return this.compile(6);
+    this.pedestrianDetail = this.compile(6);
   }
 
   compilePerpetrators() {
-    return this.compile(7)
+    this.perpetrator = this.compile(7)
   }
 
   compileAccidentType() {
-    return this.compile(8);
+    this.accitdentType = this.compile(8);
   }
 
   compileMedical() {
-    return this.compile(9);
+    this.medical = this.compile(9);
   }
 
   /**
@@ -43,6 +87,6 @@ export default class generator {
     let rows = _.uniq(_.pluck(this.rows, index));
     let data = {};
     _.assign(data, rows);
-    return data;
+    return _.invert(data);
   }
 }
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
+
 import Parser from './parser';
+import Generator from './generator';
 
 const parser = new Parser(__dirname + '/data.xls');
 let rows = parser.parse();
+
+const generator = new Generator(rows);
+generator.generate();
+generator.make('./dest/icd.json');
